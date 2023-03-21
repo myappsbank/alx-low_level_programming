b7b102cf9aedc788559b4fdfc329f9263d1bd32f
Femi Pedro <femiped@gmail.com>
2023-03-20 19:58:40 -0700

    Print alphabets more advanced

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,13 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z' ; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
 		putchar(a);
-	for (a = 'A'; a <= 'Z'; a++)
-		putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
