580a7f4524a8d7b3891c860a7b312f52ff1d0b22
Femi Pedro <femiped@gmail.com>
2023-03-22 19:29:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * _putchar writes the character c to stdout main - Entry point
+ * @C: The character to print
+ *
+ * Return: Always returns 0
+ *
+ */
+
+int main(void)
+{
+putchar('\n');
+return (0);
+}
